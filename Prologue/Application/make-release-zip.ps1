bb5cd4b411515f49388c6a5f2f7f5f4ae549607a
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2020-12-19 15:21:07 +0900

    zip化用フォルダ名を変更

--- Prologue/Application/make-release-zip.ps1
+++ Prologue/Application/make-release-zip.ps1
@@ -2,7 +2,17 @@ $scriptPath = $MyInvocation.MyCommand.Path
 
 $parentPath = Split-Path -Parent $scriptPath
 
+
+$originalName = $parentPath + "\Prologue-For-Zip"
+$fixedName = $parentPath + "\Prologue"
+
+Rename-Item -Path $originalName $fixedName
+
+
 $From = $parentPath + "\Prologue"
 $To = $parentPath + "\Prologue.zip"
 
 Compress-Archive -Path $From -DestinationPath $To -Force
+
+
+Rename-Item -Path $fixedName $originalName
