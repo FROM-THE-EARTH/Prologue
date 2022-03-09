b982ff57ba6e29a0738e033234ecdea17c59c6d7
Ekyu Kondo <kondo.eque@gmail.com>
2022-03-09 15:01:07 +0900

    fix(map): fix map origin

--- src/env/Map.hpp
+++ src/env/Map.hpp
@@ -39,16 +39,16 @@ struct MapData {
 
 namespace Map {
     const MapData NoshiroLand(
-        "nosiro_land", MapType::NOSIRO_LAND, 8.9, 40.138624, 139.984906, "nosiro_land.png", 1.0, 1.0, -850, -1200);
+        "nosiro_land", MapType::NOSIRO_LAND, 8.9, 40.138624, 139.984906, "nosiro_land.png", 1.0, 1.0, -730, -1140);
 
     const MapData NoshiroSea(
         "nosiro_sea", MapType::NOSIRO_SEA, 8.94, 40.242865, 140.010450, "nosiro_sea.png", 7.0, 7.0, -8700, -3650);
 
     const MapData IzuLand(
-        "izu_land", MapType::IZU_LAND, 7.53, 34.735972, 139.420944, "izu_land.png", 1.00, 1.00, -820, -950);
+        "izu_land", MapType::IZU_LAND, 7.53, 34.735972, 139.420944, "izu_land.png", 1.00, 1.00, -750, -820);
 
     const MapData IzuSea(
-        "izu_sea", MapType::IZU_SEA, 7.53, 34.680197, 139.439090, "izu_sea.png", 5.77, 5.77, -3080, -6210);
+        "izu_sea", MapType::IZU_SEA, 7.53, 34.680197, 139.439090, "izu_sea.png", 5.77, 5.77, -2380, -5640);
 
     std::optional<MapData> GetMap(const std::string& key);
 
