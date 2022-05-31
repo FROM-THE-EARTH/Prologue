20fe98a02fe2596fef6c6cf3fd77eaa360ed5a53
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-05-31 19:25:17 +0900

    feat(env): add environment.magnetic_declination key

--- src/env/Environment.cpp
+++ src/env/Environment.cpp
@@ -8,8 +8,9 @@ void Environment::initialize(const std::string& filename) {
     boost::property_tree::ptree pt;
     boost::property_tree::read_json("input/json/" + filename, pt);
 
-    place         = JsonUtils::GetValue<std::string>(pt, "environment.place");
-    railLength    = JsonUtils::GetValueExc<double>(pt, "environment.rail_len");
-    railAzimuth   = JsonUtils::GetValueExc<double>(pt, "environment.rail_azi");
-    railElevation = JsonUtils::GetValueExc<double>(pt, "environment.rail_elev");
+    place               = JsonUtils::GetValue<std::string>(pt, "environment.place");
+    magneticDeclination = JsonUtils::GetOptional<double>(pt, "environment.magnetic_declination");
+    railLength          = JsonUtils::GetValueExc<double>(pt, "environment.rail_len");
+    railAzimuth         = JsonUtils::GetValueExc<double>(pt, "environment.rail_azi");
+    railElevation       = JsonUtils::GetValueExc<double>(pt, "environment.rail_elev");
 }
