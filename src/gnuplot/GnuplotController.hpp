e9f5cfc8eac2306c13f19a1b3d56cd832241b8d2
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-08-05 16:59:42 +0900

    docs(GnuplotController): add comments

--- src/gnuplot/GnuplotController.hpp
+++ src/gnuplot/GnuplotController.hpp
@@ -1,3 +1,8 @@
+﻿// ------------------------------------------------
+// Gnuplotはpopenでプロセスを開いて操作する
+// そのGnuplotプロセス操作用のクラス
+// ------------------------------------------------
+
 #pragma once
 
 #include <memory>
@@ -7,6 +12,8 @@
 
 class GnuplotController {
 private:
+    // std::unique_ptr のデリータ
+    // デストラクタが呼ばれたタイミングでoperator()が実行される
     struct PtrDeleter {
         void operator()(FILE* p) const {
             PCLOSE(p);
@@ -17,20 +24,24 @@ private:
     std::unique_ptr<FILE, PtrDeleter> m_ptr = nullptr;
 
 public:
+    // 新たにプロセスを開く
     bool open() {
         m_ptr = std::unique_ptr<FILE, PtrDeleter>(POPEN("gnuplot", "w"));
         return m_ptr != nullptr;
     }
 
+    // プロセスを閉じる
     void close() {
         flush();
         m_ptr.release();
     }
 
+    // 現時点までのバッファを書き込み
     void flush() {
         fflush(m_ptr.get());
     }
 
+    // コマンドを入力
     template <class... Args>
     void send(std::string_view command, const Args&... args) {
         fprintf(m_ptr.get(), command.ends_with('\n') ? command.data() : (std::string(command) + '\n').c_str(), args...);
