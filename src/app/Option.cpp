1a723cac2a07bff6cc0c1398a11c4e931075547b
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-08-12 16:13:03 +0900

    chore(option): show warning when specify unavailable options

--- src/app/Option.cpp
+++ src/app/Option.cpp
@@ -4,24 +4,31 @@
 
 #include "Option.hpp"
 
-#include <iostream>
 #include <string>
+
+#include "app/CommandLine.hpp"
 
 namespace CommandLineOption {
     Option ParseArgs(int argc, char* argv[]) {
         Option option;
 
         for (int i = 0; i < argc; i++) {
-            const std::string opt = argv[i];
+            const std::string arg = argv[i];
 
-            if (opt == "--no-save") {
-                option.saveResult = false;
-            } else if (opt == "--no-plot") {
-                option.plotResult = false;
-            } else if (opt == "--dry-run") {
-                option.dryRun = true;
-            } else if (opt == "--open-result") {
-                option.openResultFolder = true;
+            if (arg.starts_with("--")) {
+                const auto opt = arg.substr(2);
+                if (opt == "no-save") {
+                    option.saveResult = false;
+                } else if (opt == "no-plot") {
+                    option.plotResult = false;
+                } else if (opt == "dry-run") {
+                    option.dryRun = true;
+                } else if (opt == "open-result") {
+                    option.openResultFolder = true;
+                } else {
+                    CommandLine::PrintInfo(PrintInfoType::Warning,
+                                           "Specified option \"" + opt + "\" is not available.");
+                }
             }
         }
 
